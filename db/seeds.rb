6f460c8ea24ba11e798d114b652bfef80cee0c9c
Oceane Leroy oceaneleroy1@gmail.com <oceaneleroy1@gmail.com>
2020-08-13 16:02:47 +0200

    created models, controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#Ingredient.create(name: "lemon")
+#Ingredient.create(name: "ice")
+#Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "Gin")
+# Ingredient.create(name: "Sweet Vermouth")
+# Ingredient.create(name: "Triple sec")
+# Ingredient.create(name: "Amaretto")
+# Ingredient.create(name: "Carbonated water")
+# Ingredient.create(name: "Coffee")
+# Ingredient.create(name: "Pisco")
+
+Ingredient.destroy_all
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+
+ingredient['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
